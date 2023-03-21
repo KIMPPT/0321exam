a596dace238c64e427b47ad23745c40dc6e30925
KIMPPT <dudghks9303@gmail.com>
2023-03-21 14:27:42 +0900

    03211427

--- js/todoadd.js
+++ js/todoadd.js
@@ -12,7 +12,7 @@ function addtext(e)
     e.preventDefault();
     let li=document.createElement("li"); //li태그 생성
     let checkbox=document.createElement("input"); //input 태그 생성
-    checkbox.type="checkbox"; //input태그의 타입을 checkbox로 변경
+    checkbox.type="checkbox"; //input태그의 타입을 checkbox로 만듬
     let button=document.createElement("button"); //버튼 태그 추가
     button.innerHTML="X"; //버튼에 들어갈 글자 추가
     let text=document.createTextNode(writetext.firstElementChild.value); //입력한 값을 텍스트 변수에 추가
@@ -31,7 +31,9 @@ function deleteli(e){
     let button=e.target; //제거 이벤트를 실행할 태그는 버튼 태그
     let li=e.target.parentNode; // 버튼 태그 부모(상위)는 li 태그
     let box=li.firstElementChild; //checkbox는 li 태그의 첫번째 자식
-    if(box.checked) finish--; //li 삭제 때 완료한 할 일이면 finish 카운트도 내려가게 설정
+    if(box.checked) finish--; 
+    //li 삭제 때 완료한 할 일이면 finish 카운트도 내려가게 설정
+    //이 설정을 하지 않으면 체크 된 것을 삭제 할 때 완료 카운트가 내려가지 않아서 값이 일치하지 않는다
     li.remove(); //li 태그 통채로 삭제
     --all; //전체 할 일 카운트 감소
     h3.innerHTML=`전체 할 일 ${all} / 완료한 할 일 ${finish}`;
@@ -39,15 +41,15 @@ function deleteli(e){
 function changecolor(e){
     let checkbox=e.target; //색 변경 이벤트를 실행할 태그는 체크박스 input 태그
     let text=checkbox.parentNode //체크박스의 부모(상위)는 li 태그
-    if(checkbox.checked) //checkbox의 체크 유무에 따라 투두 입력으로 온 문장 색 조절
+    if(checkbox.checked) //checkbox의 체크 유무에 따라 투두 입력으로 온 문장 색 조절+완료한 일 카운트 증감 조절
     {
         text.style.color="white";
-        finish++;
+        finish++; //체크하기 때문에 완료한 일 카운트 증가
     }
     else 
     {
         text.style.color="";
-        finish--;
+        finish--; //체크를 해제하기 때문에 완료한 일 카운트 감소
     }
     h3.innerHTML=`전체 할 일 ${all} / 완료한 할 일 ${finish}`;
 }
